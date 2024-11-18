b8cf8662456269713f16de8fb6f59395f6a1a300
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-11-18 09:56:47 +0500

    Next-js Basic
    Basic by making Login & Signup page.

---
+++ next-js-basic/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+
+
+export default function LoginPage(){
+    return(
+        <>
+        <h1>This is Login Page</h1>
+        </>
+    )
+}
---
+++ next-js-basic/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+export default function Home(){
+  return(
+    <>
+    <h1>BASIC ROUTING | MAKE NEW PAGE</h1>
+    <h2>We are Studying File Based Routing</h2>
+    <p>page.tx is a route page for all others.</p>
+    </>
+  )
+}
